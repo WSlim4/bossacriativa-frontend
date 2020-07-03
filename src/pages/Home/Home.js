0dea7fbeab1a111bd03efde07afa2f566643bdd5
wesley <wesleyjoga500@gmail.com>
2020-07-02 23:32:40 -0300

    #conserto

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -123,7 +123,7 @@ function Home() {
             {workshops.map(workshop =>
               <div style={{ backgroundColor: "#E7C032"}} onClick={()=>{ history.push(`/oficina/${workshop.id}`)}}>
                 <div className="div-img" style={{backgroundImage: `url(${workshop.img_url})`}}/>
-                <h6>{workshop.artist_name}</h6>
+                <h6 style={{backgroundColor: `${workshop.theme_color}`}}>{workshop.artist_name}</h6>
                 <p>{workshop.description}</p>
               </div>
             )}
